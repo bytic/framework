64233c76d48172ff67811c2df3b03a529a2c2b85
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-08-01 18:06:38 +0300

    add Db Manager

---
+++ src/Database/Manager.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Nip\Database;
+
+use Nip\Bootstrap as Bootstrap;
+
+class Manager
+{
+
+    /**
+     * @var Bootstrap
+     */
+    protected $_bootstrap;
+
+    /**
+     * @return Bootstrap
+     */
+    public function getBootstrap()
+    {
+        return $this->_bootstrap;
+    }
+
+    /**
+     * @param Bootstrap $bootstrap
+     */
+    public function setBootstrap($bootstrap)
+    {
+        $this->_bootstrap = $bootstrap;
+    }
+
+    protected $_connections = array();
+
+    public function newConnectionFromConfig($config)
+    {
+        $connection = $this->createNewConnection(
+            $config->adapter,
+            $config->host,
+            $config->user,
+            $config->password,
+            $config->name);
+        return $connection;
+    }
+
+    public function createNewConnection($adapter, $host, $user, $password, $database)
+    {
+
+        try {
+            $connection = $this->newConnection();
+
+            $adapter = $connection->newAdapter($adapter);
+            $connection->setAdapter($adapter);
+
+            $connection->connect($host, $user, $password, $database);
+            $this->initNewConnection($connection);
+
+        } catch (Nip_DB_Exception $e) {
+            echo '<h1>Error connecting to database</h1>';
+            if (Nip_Staging::instance()->getStage()->inTesting()) {
+                echo '<h4>' . $e->getMessage() . '</h4>';
+                $e->log();
+            }
+            die();
+        }
+
+        return $connection;
+    }
+
+    public function initNewConnection($connection)
+    {
+        if ($this->getBootstrap()->getDebugBar()->isEnabled()) {
+            $this->getBootstrap()->getDebugBar()->initDatabaseAdapter($connection->getAdapter());
+        }
+    }
+
+    public function newConnection()
+    {
+        return new \Nip\Database\Connection();
+    }
+
+}
