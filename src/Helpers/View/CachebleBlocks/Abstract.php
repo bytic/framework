131727262974b9abd3fe4baed7eec463bbf65c7f
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-08-04 14:46:42 +0300

    update Cacheble Blocks

--- src/Helpers/View/CachebleBlocks/Abstract.php
+++ src/Helpers/View/CachebleBlocks/Abstract.php
@@ -2,96 +2,103 @@
 
 namespace Nip\Helpers\View\CachebleBlocks;
 
-use Nip_File_System as FileSystem;
 use Nip\Staging as Staging;
+use Nip_File_System as FileSystem;
 
-class AbstractBlock {
+class AbstractBlock
+{
 
-	protected $_name;
-    
+    protected $_name;
+
     /** $_model Nip_Record */
-	protected $_model;
-	protected $_manager;
-	protected $_viewPath;
+    protected $_model;
+    protected $_manager;
+    protected $_viewPath;
 
-	public function setName($name) {
+    public function setName($name)
+    {
         $this->_name = $name;
         return $this;
-	}
+    }
 
-	public function setModel($model) {
+    public function setModel($model)
+    {
         $this->_model = $model;
         return $this;
-	}
+    }
 
-	public function setManager($manager) {
+    public function setManager($manager)
+    {
         $this->_manager = $manager;
         return $this;
-	}
+    }
 
-	public function setViewPath($path) {
+    public function setViewPath($path)
+    {
         $this->_viewPath = $path;
         return $this;
-	}
-       
-	public function render() {        
+    }
+
+    public function render()
+    {
         $file = $this->filePath();
 //        echo gzuncompress(file_get_contents($file));
-        echo readfile ($file);
-	}
-    
+        readfile($file);
+    }
+
     public function valid($ttl)
     {
         $ttl = $ttl !== null ? $ttl : $this->_ttl;
-		if ($this->exists()) {
-			if (!is_int($ttl)) {
-				return true;
-			}
-			$fmtime = filemtime($this->filePath());
-			if (($fmtime + $ttl) > time()) {
-				return true;
-			}
-		}
-		return false;        
+        if ($this->exists()) {
+            if (!is_int($ttl)) {
+                return true;
+            }
+            $fmtime = filemtime($this->filePath());
+            if (($fmtime + $ttl) > time()) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
-    public function regenerate() {
+    public function regenerate()
+    {
         $file = $this->filePath();
         $content = $this->_manager->getView()->load($this->_viewPath, array(), true);
-        
-		$dir = dirname($file);
+
+        $dir = dirname($file);
         if (!is_dir($dir)) {
             FileSystem::instance()->createDirectory($dir, 0777);
         }
 
         $content = $this->_manager->getView()->HTML()->compress($content);
 //        $content = gzcompress($content);
-		if (file_put_contents($file, $content)) {
-			chmod($file, 0777);
-			return true;
-		} else {
-			$message = "Cannot open CachebleBlocks file for writing: ";
-			if (Staging::instance()->getStage()->inTesting()) {
-				$message .= " [ ".$file." ] ";
-			}
-			die($message);
-		}        
-	}
+        if (file_put_contents($file, $content)) {
+            chmod($file, 0777);
+            return true;
+        } else {
+            $message = "Cannot open CachebleBlocks file for writing: ";
+            if (Staging::instance()->getStage()->inTesting()) {
+                $message .= " [ " . $file . " ] ";
+            }
+            die($message);
+        }
+    }
 
-	public function exists()
-	{
-		return file_exists($this->filePath());
-	}
-       
-	public function filePath()
-	{
-        $fileName = str_replace('/','+',$this->_viewPath); 
-		return $this->cachePath().$fileName.'.html';
-	}
+    public function exists()
+    {
+        return file_exists($this->filePath());
+    }
 
-	public function cachePath()
-	{
-		return $this->_model->getCacheBlocksPath();
-	}
+    public function filePath()
+    {
+        $fileName = str_replace('/', '+', $this->_viewPath);
+        return $this->cachePath() . $fileName . '.html';
+    }
+
+    public function cachePath()
+    {
+        return $this->_model->getCacheBlocksPath();
+    }
 }
