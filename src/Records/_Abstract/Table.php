d68b67dd328a9b66cb2ade3f8fc98c87fa4323a7
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-08-08 22:49:35 +0300

    fix clone relations

--- src/Records/_Abstract/Table.php
+++ src/Records/_Abstract/Table.php
@@ -1103,7 +1103,7 @@ abstract class Table
      */
     public function cloneRelations($from, $to)
     {
-        $relations = $this->getRelations();
+        $relations = $from->getManager()->getRelations();
         foreach ($relations as $name=>$relation) {
             /** @var \Nip\Records\Relations\HasMany $relation */
             if ($relation->getType() != 'belongsTo') {
