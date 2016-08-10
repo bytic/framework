e8bf9a6e5539f4c5a65b15df641259b31524cf1c
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-08-10 09:17:10 +0300

    deprecate getOptions in select form

--- src/form/element/Select.php
+++ src/form/element/Select.php
@@ -1,19 +1,22 @@
 <?php
-class Nip_Form_Element_Select extends Nip_Form_Element_Abstract {
+
+class Nip_Form_Element_Select extends Nip_Form_Element_Abstract
+{
 
     protected $_type = 'select';
-    protected $_optionsElementsElements = array();
+    protected $_optionsElements = array();
     protected $_values = array();
 
     /**
      * @return Nip_Form_Element_Select
      */
-    public function addOptionsArray($options, $valueKey, $labelKey) {
+    public function addOptionsArray($options, $valueKey, $labelKey)
+    {
         foreach ($options as $key => $option) {
-            $option = (object) $option;
+            $option = (object)$option;
 
-            $oValue    = $option->$valueKey;
-            $oLabel   = $option->$labelKey;
+            $oValue = $option->$valueKey;
+            $oLabel = $option->$labelKey;
             $oDisabled = $option->disabled;
 
             $atribs = array(
@@ -32,7 +35,8 @@ class Nip_Form_Element_Select extends Nip_Form_Element_Abstract {
     /**
      * @return Nip_Form_Element_Select
      */
-    public function addOptions(array $array) {
+    public function addOptions(array $array)
+    {
         foreach ($array as $value => $label) {
             $this->addOption($value, $label);
         }
@@ -43,7 +47,8 @@ class Nip_Form_Element_Select extends Nip_Form_Element_Abstract {
     /**
      * @return Nip_Form_Element_Select
      */
-    public function addOption($value, $label) {
+    public function addOption($value, $label)
+    {
         if (is_array($label)) {
             $option = $label;
         } else {
@@ -59,7 +64,8 @@ class Nip_Form_Element_Select extends Nip_Form_Element_Abstract {
     /**
      * @return Nip_Form_Element_Select
      */
-    public function appendOptgroupOption($optgroup, $value, $label) {
+    public function appendOptgroupOption($optgroup, $value, $label)
+    {
         if (is_array($label)) {
             $option = $label;
         } else {
@@ -72,15 +78,30 @@ class Nip_Form_Element_Select extends Nip_Form_Element_Abstract {
         return $this;
     }
 
-    public function getOptions() {
+    /**
+     * @deprecated to stop confusion from select options and element options
+     * @return array
+     */
+    public function getOptions()
+    {
         return $this->_optionsElements;
     }
 
-    public function setValue($value) {
+    /**
+     * @return array
+     */
+    public function getOptionsElements()
+    {
+        return $this->_optionsElements;
+    }
+
+    public function setValue($value)
+    {
 
         if (in_array($value, $this->_values)) {
             return parent::setValue($value);
         }
+
         return false;
     }
 
