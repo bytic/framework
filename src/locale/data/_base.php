8159b0f6b67021d4c9f7a160206539d68c3199ee
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-08-17 11:49:20 +0300

    add new locale data

--- src/locale/data/_base.php
+++ src/locale/data/_base.php
@@ -3,9 +3,11 @@
 $_data['time'] = array(
     'dateTimeFormat' => 'm/d/Y H:m',
     'dateFormat' => 'm/d/Y',
+    'dateStringFormatMini' => '%d/%b/%y',
     'dateStringFormatShort' => '%d %b %Y',
     'dateStringFormat' => '%d %B %Y',
     'dayDateStringFormat' => '%A, %B %d, %Y',
+    'dayDateTimeStringFormat' => '%A, %d %B, %Y %X',
     'firstWeekDay' => 0,
     'timeFormat' => 'H:m',
 );
--- src/locale/data/ro.php
+++ src/locale/data/ro.php
@@ -5,6 +5,7 @@ $_import = '_base';
 $_data['time'] = array(
     'dateTimeFormat' => 'd/m/Y H:m',
     'dateFormat' => 'd/m/Y',
+    'dateStringFormatMini' => '%d/%b/%y',
     'dateStringFormat' => '%d %B %Y',
     'dayDateStringFormat' => '%A, %d %B, %Y',
     'firstWeekDay' => 0,
