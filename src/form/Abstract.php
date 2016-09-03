03d64b2208e28bd1e1731374a920b64b6f97d4d3
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-09-03 19:52:44 +0300

    initial add form helper methods

--- src/form/Abstract.php
+++ src/form/Abstract.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Class Nip_Form_Abstract
+ *
+ *
+ * @method addSelect($name, $label = false, $type = 'input', $isRequired = false)
+ * @method addInput($name, $label = false, $type = 'input', $isRequired = false)
+ * @method addDateinput($name, $label = false, $type = 'input', $isRequired = false)
+ * @method addBsRadioGroup($name, $label = false, $type = 'input', $isRequired = false)
+ * @method addTextarea($name, $label = false, $type = 'input', $isRequired = false)
+ *
+ */
 abstract class Nip_Form_Abstract
 {
     const ENCTYPE_URLENCODED = 'application/x-www-form-urlencoded';
@@ -41,6 +52,32 @@ abstract class Nip_Form_Abstract
         $this->postInit();
     }
 
+    public function init()
+    {
+        $this->setAction(current_url());
+    }
+
+    public function setAction($action)
+    {
+        return $this->setAttrib('action', (string)$action);
+    }
+
+    /**
+     * @return Nip_Form
+     */
+    public function setAttrib($key, $value)
+    {
+        $key = (string)$key;
+        $this->_attribs[$key] = $value;
+
+        return $this;
+    }
+
+    public function postInit()
+    {
+
+    }
+
     public function __call($name, $arguments)
     {
         if (strpos($name, 'add') === 0) {
@@ -59,25 +96,6 @@ abstract class Nip_Form_Abstract
         trigger_error('Call to undefined method: [' . $name . ']', E_USER_ERROR);
     }
 
-    public function __get($name)
-    {
-        $element = $this->getElement($name);
-        if ($element) {
-            return $element;
-        }
-        return NULL;
-    }
-
-    public function init()
-    {
-        $this->setAction(current_url());
-    }
-
-    public function postInit()
-    {
-
-    }
-
     public function add($name, $label = false, $type = 'input', $isRequired = false)
     {
         $label = ($label) ? $label : ucfirst($name);
@@ -86,7 +104,77 @@ abstract class Nip_Form_Abstract
             ->setLabel($label)
             ->setRequired($isRequired);
         $this->addElement($element);
+
         return $this;
+    }
+
+    /**
+     * @return Nip_Form_Element_Abstract
+     */
+    public function getNewElement($type)
+    {
+        $className = $this->getElementClassName($type);
+
+        return $this->getNewElementByClass($className);
+    }
+
+    /**
+     * @return Nip_Form_Element_Abstract
+     */
+    public function getElementClassName($type)
+    {
+        return 'Nip_Form_Element_'.ucfirst($type);
+    }
+
+    /**
+     * @return Nip_Form_Element_Abstract
+     */
+    public function getNewElementByClass($className)
+    {
+        $element = new $className($this);
+
+        return $this->initNewElement($element);
+    }
+
+    /**
+     * @return Nip_Form_Element_Abstract
+     */
+    public function initNewElement($element)
+    {
+        $element->setForm($this);
+
+        return $element;
+    }
+
+    public function addElement(Nip_Form_Element_Abstract $element)
+    {
+        $name = $element->getUniqueId();
+        $this->_elements[$name] = $element;
+        $this->_elementsLabel[$element->getLabel()] = $name;
+        $this->_elementsOrder[] = $name;
+        return $this;
+    }
+
+    public function __get($name)
+    {
+        $element = $this->getElement($name);
+        if ($element) {
+            return $element;
+        }
+
+        return null;
+    }
+
+    /**
+     * @return Nip_Form_Element_Abstract
+     */
+    public function getElement($name)
+    {
+        if (array_key_exists($name, $this->_elements)) {
+            return $this->_elements[$name];
+        }
+
+        return null;
     }
 
     public function addCustom($className, $name, $label = false, $isRequired = false)
@@ -97,15 +185,6 @@ abstract class Nip_Form_Abstract
             ->setLabel($label)
             ->setRequired($isRequired);
         $this->addElement($element);
-        return $this;
-    }
-
-    public function addElement(Nip_Form_Element_Abstract $element)
-    {
-        $name = $element->getUniqueId();
-        $this->_elements[$name] = $element;
-        $this->_elementsLabel[$element->getLabel()] = $name;
-        $this->_elementsOrder[] = $name;
         return $this;
     }
 
@@ -147,6 +226,14 @@ abstract class Nip_Form_Abstract
         return $this;
     }
 
+    public function newDisplayGroup()
+    {
+        $group = new Nip_Form_DisplayGroup();
+        $group->setForm($this);
+
+        return $group;
+    }
+
     /**
      * @return Nip_Form_DisplayGroup
      */
@@ -156,13 +243,6 @@ abstract class Nip_Form_Abstract
             return $this->_displayGroups[$name];
         }
         return null;
-    }
-
-    public function newDisplayGroup()
-    {
-        $group = new Nip_Form_DisplayGroup();
-        $group->setForm($this);
-        return $group;
     }
 
     public function getDisplayGroups()
@@ -186,17 +266,6 @@ abstract class Nip_Form_Abstract
     {
         if (array_key_exists($name, $this->_buttons)) {
             return $this->_buttons[$name];
-        }
-        return null;
-    }
-
-    /**
-     * @return Nip_Form_Element_Abstract
-     */
-    public function getElement($name)
-    {
-        if (array_key_exists($name, $this->_elements)) {
-            return $this->_elements[$name];
         }
         return null;
     }
@@ -244,16 +313,6 @@ abstract class Nip_Form_Abstract
         return $this;
     }
 
-    public function getElements()
-    {
-        $return = array();
-        foreach ($this->_elementsOrder as $current) {
-            $return[$current] = $this->_elements[$current];
-        }
-
-        return $return;
-    }
-
     public function getButtons()
     {
         return $this->_buttons;
@@ -278,41 +337,6 @@ abstract class Nip_Form_Abstract
             $elements[$element->getUniqueId()] = $element;
         }
         return $elements;
-    }
-
-    /**
-     * @return Nip_Form_Element_Abstract
-     */
-    public function getNewElement($type)
-    {
-        $className = $this->getElementClassName($type);
-        return $this->getNewElementByClass($className);
-    }
-
-    /**
-     * @return Nip_Form_Element_Abstract
-     */
-    public function getNewElementByClass($className)
-    {
-        $element = new $className($this);
-        return $this->initNewElement($element);
-    }
-
-    /**
-     * @return Nip_Form_Element_Abstract
-     */
-    public function initNewElement($element)
-    {
-        $element->setForm($this);
-        return $element;
-    }
-
-    /**
-     * @return Nip_Form_Element_Abstract
-     */
-    public function getElementClassName($type)
-    {
-        return 'Nip_Form_Element_' . ucfirst($type);
     }
 
     /**
@@ -346,6 +370,16 @@ abstract class Nip_Form_Abstract
         return $this;
     }
 
+    public function getAttrib($key)
+    {
+        $key = (string)$key;
+        if (!isset($this->_attribs[$key])) {
+            return null;
+        }
+
+        return $this->_attribs[$key];
+    }
+
     public function removeClass()
     {
         $removeClasses = func_get_args();
@@ -367,13 +401,28 @@ abstract class Nip_Form_Abstract
         return in_array($class, explode(' ', $this->getAttrib('class')));
     }
 
+    public function getAttribs()
+    {
+        return $this->_attribs;
+    }
+
+    /**
+     * @param  array $attribs
+     * @return Nip_Form
+     */
+    public function setAttribs(array $attribs)
+    {
+        $this->clearAttribs();
+
+        return $this->addAttribs($attribs);
+    }
+
     /**
      * @return Nip_Form
      */
-    public function setAttrib($key, $value)
+    public function clearAttribs()
     {
-        $key = (string)$key;
-        $this->_attribs[$key] = $value;
+        $this->_attribs = array();
         return $this;
     }
 
@@ -386,32 +435,8 @@ abstract class Nip_Form_Abstract
         foreach ($attribs as $key => $value) {
             $this->setAttrib($key, $value);
         }
+
         return $this;
-    }
-
-    /**
-     * @param  array $attribs
-     * @return Nip_Form
-     */
-    public function setAttribs(array $attribs)
-    {
-        $this->clearAttribs();
-        return $this->addAttribs($attribs);
-    }
-
-    public function getAttrib($key)
-    {
-        $key = (string)$key;
-        if (!isset($this->_attribs[$key])) {
-            return null;
-        }
-
-        return $this->_attribs[$key];
-    }
-
-    public function getAttribs()
-    {
-        return $this->_attribs;
     }
 
     /**
@@ -427,20 +452,6 @@ abstract class Nip_Form_Abstract
         return false;
     }
 
-    /**
-     * @return Nip_Form
-     */
-    public function clearAttribs()
-    {
-        $this->_attribs = array();
-        return $this;
-    }
-
-    public function setAction($action)
-    {
-        return $this->setAttrib('action', (string)$action);
-    }
-
     public function setMethod($method)
     {
         if (in_array($method, $this->_methods)) {
@@ -449,19 +460,19 @@ abstract class Nip_Form_Abstract
         trigger_error('Method is not valid', E_USER_ERROR);
     }
 
+    public function execute()
+    {
+        if ($this->submited()) {
+            return $this->processRequest();
+        }
+        return false;
+    }
+
     public function submited()
     {
         $request = $this->getAttrib('method') == 'post' ? $_POST : $_GET;
         if (count($request)) {
             return true;
-        }
-        return false;
-    }
-
-    public function execute()
-    {
-        if ($this->submited()) {
-            return $this->processRequest();
         }
         return false;
     }
@@ -475,39 +486,12 @@ abstract class Nip_Form_Abstract
         return false;
     }
 
-    public function process()
-    {
-
-    }
-
     public function validate()
     {
         $request = $this->getAttrib('method') == 'post' ? $_POST : $_GET;
         $this->getDataFromRequest($request);
         $this->processValidation();
         return $this->isValid();
-    }
-
-    public function processValidation()
-    {
-        $elements = $this->getElements();
-        if (is_array($elements)) {
-            foreach ($elements as $name => $element) {
-                $element->validate();
-            }
-        }
-    }
-
-    protected function getData()
-    {
-        $data = array();
-        $elements = $this->getElements();
-        if (is_array($elements)) {
-            foreach ($elements as $name => $element) {
-                $data[$name] = $element->getValue();
-            }
-        }
-        return $data;
     }
 
     protected function getDataFromRequest($request)
@@ -537,6 +521,26 @@ abstract class Nip_Form_Abstract
         }
     }
 
+    public function getElements()
+    {
+        $return = array();
+        foreach ($this->_elementsOrder as $current) {
+            $return[$current] = $this->_elements[$current];
+        }
+
+        return $return;
+    }
+
+    public function processValidation()
+    {
+        $elements = $this->getElements();
+        if (is_array($elements)) {
+            foreach ($elements as $name => $element) {
+                $element->validate();
+            }
+        }
+    }
+
     public function isValid()
     {
         return count($this->getErrors()) > 0 ? false : true;
@@ -548,21 +552,9 @@ abstract class Nip_Form_Abstract
         return $errors;
     }
 
-    public function addError($message)
-    {
-        $this->_messages['error'][] = $message;
-        return $this;
-    }
-
     public function getMessagesType($type = 'error')
     {
         return $this->_messages[$type];
-    }
-
-    public function addMessage($message, $type = 'error')
-    {
-        $this->_messages[$type][] = $message;
-        return $this;
     }
 
     public function getElementsErrors()
@@ -577,6 +569,25 @@ abstract class Nip_Form_Abstract
         return $errors;
     }
 
+    public function process()
+    {
+
+    }
+
+    public function addError($message)
+    {
+        $this->_messages['error'][] = $message;
+
+        return $this;
+    }
+
+    public function addMessage($message, $type = 'error')
+    {
+        $this->_messages[$type][] = $message;
+
+        return $this;
+    }
+
     public function getMessages()
     {
         $messages = $this->_messages;
@@ -589,20 +600,14 @@ abstract class Nip_Form_Abstract
         return $this->_messageTemplates[$name];
     }
 
-    public function render()
-    {
-        return $this->getRenderer()->render();
-    }
-
     /**
      * @return Nip_Form_Renderer
      */
-    public function getRenderer()
+    public function setRendererType($type)
     {
-        if (!$this->_renderer) {
-            $this->_renderer = $this->getNewRenderer();
-        }
-        return $this->_renderer;
+        $this->_renderer = $this->getNewRenderer($type);
+
+        return $this;
     }
 
     /**
@@ -614,15 +619,6 @@ abstract class Nip_Form_Abstract
         $renderer = new $name();
         $renderer->setForm($this);
         return $renderer;
-    }
-
-    /**
-     * @return Nip_Form_Renderer
-     */
-    public function setRendererType($type)
-    {
-        $this->_renderer = $this->getNewRenderer($type);
-        return $this;
     }
 
     public function getCache($key)
@@ -655,6 +651,23 @@ abstract class Nip_Form_Abstract
         return $this->render();
     }
 
+    public function render()
+    {
+        return $this->getRenderer()->render();
+    }
+
+    /**
+     * @return Nip_Form_Renderer
+     */
+    public function getRenderer()
+    {
+        if (!$this->_renderer) {
+            $this->_renderer = $this->getNewRenderer();
+        }
+
+        return $this->_renderer;
+    }
+
     public function getControllerView()
     {
         if (!$this->_controllerView) {
@@ -662,6 +675,19 @@ abstract class Nip_Form_Abstract
         }
 
         return $this->_controllerView;
+    }
+
+    protected function getData()
+    {
+        $data = array();
+        $elements = $this->getElements();
+        if (is_array($elements)) {
+            foreach ($elements as $name => $element) {
+                $data[$name] = $element->getValue();
+            }
+        }
+
+        return $data;
     }
 
 }
