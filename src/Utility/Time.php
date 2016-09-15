ec6ecbd3a15841b0ac94a03b5358ab5be34a874c
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-09-15 10:04:56 +0300

    update time utility

--- src/Utility/Time.php
+++ src/Utility/Time.php
@@ -9,23 +9,21 @@ class Time
     protected $_parts = null;
     protected $_seconds = null;
 
-    public function getSeconds()
+    public static function fromString($string)
     {
-        if ($this->_seconds === null) {
-            $this->parseSeconds();
-        }
-        return $this->_seconds;
+        $o = new self();
+        $o->_value = $string;
+        $o->parseSeconds();
+
+        return $o;
     }
 
-    public function parseSeconds()
+    public static function fromSeconds($seconds)
     {
-        $parts = $this->getParts();
-        if (count($parts) == 3) {
-            $this->_seconds = 0;
-            $this->_seconds += $parts['h'] * 3600;
-            $this->_seconds += $parts['m'] * 60;
-            $this->_seconds += $parts['s'];
-        }
+        $o = new self();
+        $o->setSeconds($seconds);
+
+        return $o;
     }
 
     public function getPart($p)
@@ -33,20 +31,13 @@ class Time
         if ($this->_parts === null) {
             $this->parseParts();
         }
-        return $this->_parts[$p];
-    }
 
-    public function getParts()
-    {
-        if ($this->_parts === null) {
-            $this->parseParts();
-        }
-        return $this->_parts;
+        return $this->_parts[$p];
     }
 
     public function parseParts()
     {
-        if ($this->_value && substr_count($this->_value, ':') ==2) {
+        if ($this->_value && substr_count($this->_value, ':') == 2) {
             list ($h, $m, $s) = explode(':', $this->_value);
             $this->_parts = array(
                 'h' => $h,
@@ -63,26 +54,56 @@ class Time
 
         if ($hours = intval((floor($seconds / 3600))) OR $return) {
             $seconds = $seconds - $hours * 3600;
-            $return .= ($return ? ' ' : '') . str_pad($hours, 2, 0, STR_PAD_LEFT) . 'h';
+            $return .= ($return ? ' ' : '').str_pad($hours, 2, 0, STR_PAD_LEFT).'h';
         }
         if ($minutes = intval((floor($seconds / 60))) OR $return) {
             $seconds = $seconds - $minutes * 60;
-            $return .= ($return ? ' ' : '') . str_pad($minutes, 2, 0, STR_PAD_LEFT) . 'm';
+            $return .= ($return ? ' ' : '').str_pad($minutes, 2, 0, STR_PAD_LEFT).'m';
         }
 
         $seconds = round($seconds, 2);
         if ($seconds) {
-            $return .= ($return ? ' ' : '') . str_pad($seconds, 2, 0, STR_PAD_LEFT) . 's';
+            $return .= ($return ? ' ' : '').str_pad($seconds, 2, 0, STR_PAD_LEFT).'s';
         }
 
         return $return;
     }
 
-    public static function fromString($string)
+    public function getSeconds()
     {
-        $o = new self();
-        $o->_value = $string;
-        $o->parseSeconds();
-        return $o;
+        if ($this->_seconds === null) {
+            $this->parseSeconds();
+        }
+
+        return $this->_seconds;
+    }
+
+    /**
+     * @param null $seconds
+     */
+    public function setSeconds($seconds)
+    {
+        $this->_seconds = $seconds;
+    }
+
+    public function parseSeconds()
+    {
+        $parts = $this->getParts();
+        if (count($parts) == 3) {
+            $seconds = 0;
+            $seconds += $parts['h'] * 3600;
+            $seconds += $parts['m'] * 60;
+            $seconds += $parts['s'];
+            $this->setSeconds($seconds);
+        }
+    }
+
+    public function getParts()
+    {
+        if ($this->_parts === null) {
+            $this->parseParts();
+        }
+
+        return $this->_parts;
     }
 }
