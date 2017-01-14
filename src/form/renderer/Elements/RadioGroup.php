cd38458ecdfdb59dbc5aa63c9373ebdd94589e3e
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-01-14 10:53:07 +0200

    refactor radioGroup

--- src/form/renderer/Elements/RadioGroup.php
+++ src/form/renderer/Elements/RadioGroup.php
@@ -1,6 +1,13 @@
 <?php
+
+/**
+ * Class Nip_Form_Renderer_Elements_RadioGroup
+ */
 class Nip_Form_Renderer_Elements_RadioGroup extends Nip_Form_Renderer_Elements_Input_Group {
-    
+
+    /**
+     * @return string
+     */
     public function generateElement() {
         $this->_checkValue();
 
