22b155e21058f782b12957b929fdd378e24ac18d
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-03-02 14:36:20 +0200

    initial add services

---
+++ src/Container/ServicesProviders/Services/ProviderRepositoryInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Nip\Container\ServiceProviders\Services;
+
+use Nip\Container\ContainerAwareInterface;
+
+/**
+ * Interface ServiceProviderAggregateInterface
+ * @package Nip\Container\ServiceProvider
+ */
+interface ProviderRepositoryInterface extends ContainerAwareInterface
+{
+    /**
+     * Add a service provider to the aggregate.
+     *
+     * @param  string|\Nip\Container\ServiceProvider\ServiceProviderInterface $provider
+     * @return $this
+     */
+    public function add($provider);
+
+    /**
+     * Determines whether a service is provided by the aggregate.
+     *
+     * @param  string $service
+     * @return boolean
+     */
+    public function provides($service);
+
+    /**
+     * Invokes the register method of a provider that provides a specific service.
+     *
+     * @param  string $service
+     * @return void
+     */
+    public function register($service);
+}
