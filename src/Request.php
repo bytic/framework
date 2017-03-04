fc6c97ad1888ffbd9323a4cf0f682f284ee3ace8
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-03-04 17:10:12 +0200

    add psr bridge to request and response

--- src/Request.php
+++ src/Request.php
@@ -4,14 +4,16 @@ namespace Nip;
 
 use ByTIC\RequestDetective\RequestDetective;
 use Nip\Http\Request\Http;
+use Nip\Http\Request\PsrBridgeTrait;
+use Psr\Http\Message\RequestInterface;
 
 /**
  * Class Request
  * @package Nip
  */
-class Request extends \Symfony\Component\HttpFoundation\Request implements \ArrayAccess
+class Request extends \Symfony\Component\HttpFoundation\Request implements \ArrayAccess, RequestInterface
 {
-
+    use PsrBridgeTrait;
     /**
      * Has the action been dispatched?
      * @var boolean
