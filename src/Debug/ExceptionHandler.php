764368274951b874966c2607714d306fe179ca1e
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-03-13 10:44:37 +0200

    add new Debug class

---
+++ src/Debug/ExceptionHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Nip\Debug;
+
+use Symfony\Component\Debug\ExceptionHandler as SymfonyExceptionHandler;
+
+/**
+ * Class ExceptionHandler
+ * @package Nip\Debug
+ */
+class ExceptionHandler extends SymfonyExceptionHandler
+{
+}
