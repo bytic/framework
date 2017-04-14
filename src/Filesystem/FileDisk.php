51d34c920692ee1af2765dc93660e28923283a16
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-04-14 15:45:23 +0300

    add method putFileAs

--- src/Filesystem/FileDisk.php
+++ src/Filesystem/FileDisk.php
@@ -3,6 +3,7 @@
 namespace Nip\Filesystem;
 
 use League\Flysystem\Filesystem as Flysystem;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * Class FlysystemAdapter
@@ -11,4 +12,31 @@ use League\Flysystem\Filesystem as Flysystem;
 class FileDisk extends Flysystem
 {
 
+    /**
+     * Store the uploaded file on the disk with a given name.
+     *
+     * @param  string $path
+     * @param  UploadedFile $file
+     * @param  string $name
+     * @param  array $options
+     * @return string|false
+     */
+    public function putFileAs($path, $file, $name, $options = [])
+    {
+        $stream = fopen($file->getRealPath(), 'r+');
+
+        // Next, we will format the path of the file and store the file using a stream since
+        // they provide better performance than alternatives. Once we write the file this
+        // stream will get closed automatically by us so the developer doesn't have to.
+        $result = $this->put(
+            $path = trim($path . '/' . $name, '/'),
+            $stream,
+            $options
+        );
+
+        if (is_resource($stream)) {
+            fclose($stream);
+        }
+        return $result ? $path : false;
+    }
 }
