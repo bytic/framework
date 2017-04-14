7b834bc32493267c3f4e605944f18608c2bd3674
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-04-14 18:06:21 +0300

    update file

--- src/Filesystem/File.php
+++ src/Filesystem/File.php
@@ -20,10 +20,68 @@ class File extends \League\Flysystem\File
     protected $url;
 
     /**
+     * @param string $name
+     * @return $this
+     */
+    public function setFileName($name)
+    {
+        $path_parts = pathinfo($this->getPath());
+        $path_parts['filename'] = $name;
+        $this->setPath(
+            $path_parts['dirname']
+            . '/' . $path_parts['filename'] . '.' . $path_parts['extension']
+        );
+
+        return $this;
+    }
+
+    /**
+     * Get File path with init check
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        if (!$this->path) {
+            $this->initPath();
+        }
+        return parent::getPath();
+    }
+
+    /**
+     * @return string
+     */
+    protected function initPath()
+    {
+        $this->setPath($this->getPathFolder() . $this->getName());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setPath($path)
+    {
+        $name = pathinfo($path, PATHINFO_BASENAME);
+        $this->setName($name);
+        return parent::setPath($path);
+    }
+
+    /**
+     * @return string
+     */
+    public function getPathFolder()
+    {
+        return '/';
+    }
+
+    /**
      * @return mixed
      */
     public function getName()
     {
+        if (!$this->name) {
+            $this->initName();
+        }
         return $this->name;
     }
 
@@ -34,10 +92,20 @@ class File extends \League\Flysystem\File
     public function setName($name)
     {
         $this->name = $name;
-//        $this->path = $this->getPath();
-//        $this->url = $this->getUrl();
-
         return $this;
+    }
+
+    protected function initName()
+    {
+        $this->name = $this->getDefaultName();
+    }
+
+    /**
+     * @return string
+     */
+    public function getDefaultName()
+    {
+        return 'file';
     }
 
     /**
