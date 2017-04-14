4b79b46128414209a967a6e3a972ddb00f4c0ade
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-04-14 15:19:31 +0300

    add method to File Class

--- src/Filesystem/File.php
+++ src/Filesystem/File.php
@@ -8,4 +8,43 @@ namespace Nip\Filesystem;
  */
 class File extends \League\Flysystem\File
 {
+
+    /**
+     * @var
+     */
+    protected $name;
+
+    /**
+     * @var
+     */
+    protected $url;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+//        $this->path = $this->getPath();
+//        $this->url = $this->getUrl();
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url ? $this->url : $this->getUrlPath() . $this->name;
+    }
 }
