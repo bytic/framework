3539089b21f751c6309d8cc5398a97f745d19536
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-07-30 22:31:37 +0300

    update class

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,5 @@
 <?php
 
-//<?php
-//// Here you can initialize variables that will be available to your tests
-//
-//\Codeception\Util\Autoload::addNamespace(
-//        'Nip\Tests',
-//        dirname(__FILE__)
-//    );
-//
 app()->share('inflector', new Nip\Inflector\Inflector());
 app()->share('app', new \Nip\Application());
 
@@ -15,6 +7,5 @@ app()->share('app', new \Nip\Application());
 define('PROJECT_BASE_PATH', __DIR__ . '/..');
 define('TEST_BASE_PATH', __DIR__);
 define('TEST_FIXTURE_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'fixtures');
-
 
 require dirname(__DIR__) . '/vendor/autoload.php';
