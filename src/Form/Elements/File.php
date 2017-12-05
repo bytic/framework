29986e58da3bad8be5121227417da586d2b017a3
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-12-05 19:57:15 +0200

    update file to return UploadedFile

--- src/Form/Elements/File.php
+++ src/Form/Elements/File.php
@@ -1,8 +1,11 @@
 <?php
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 class Nip_Form_Element_File extends Nip_Form_Element_Input_Abstract
 {
-    protected $_value;
-
+    protected $fileData = null;
+    protected $fileObject = null;
 
     public function init()
     {
@@ -11,9 +14,59 @@ class Nip_Form_Element_File extends Nip_Form_Element_Input_Abstract
         $this->getForm()->setAttrib('enctype', 'multipart/form-data');
     }
 
-    public function getValue($requester = 'abstract')
+    /** @noinspection PhpMissingParentCallCommonInspection
+     * @param $request
+     * @return void
+     */
+    public function getDataFromRequest($request)
     {
-        if (!$this->_value) {
+        $object = $this->getFileObject();
+        $this->setValue($object);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFileObject()
+    {
+        if ($this->fileObject === null) {
+            $this->fileObject = $this->generateObjectFromData($this->getFileData());
+        }
+
+        return $this->fileObject;
+    }
+
+    /**
+     * @param $data
+     * @return bool|UploadedFile
+     */
+    protected function generateObjectFromData($data)
+    {
+        if (!is_array($data)) {
+            return false;
+        }
+
+        $keys = array_keys($data);
+        if ($keys !== ['error', 'name', 'size', 'tmp_name', 'type']) {
+            return false;
+        }
+
+        if (UPLOAD_ERR_NO_FILE == $data['error']) {
+            return false;
+        }
+
+        $file = new UploadedFile($data['tmp_name'], $data['name'], $data['type'], $data['size'],
+            $data['error']);
+
+        return $file;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getFileData()
+    {
+        if (!$this->fileData) {
             $name = $this->getName();
             $name = str_replace(']', '', $name);
             $parts = explode('[', $name);
@@ -21,17 +74,18 @@ class Nip_Form_Element_File extends Nip_Form_Element_Input_Abstract
             if (count($parts) > 1) {
                 if ($_FILES[$parts[0]]) {
                     $fileData = [];
-                    foreach ($_FILES[$parts[0]] as $key=>$data) {
+                    foreach ($_FILES[$parts[0]] as $key => $data) {
                         $fileData[$key] = $data[$parts[1]];
                     }
-                    $this->_value = $fileData;
+                    $this->fileData = $fileData;
                 } else {
-                    $this->_value = null;
+                    $this->fileData = null;
                 }
             } else {
-                $this->_value = $_FILES[$name];
+                $this->fileData = $_FILES[$name];
             }
         }
-        return $this->_value;
+
+        return $this->fileData;
     }
 }
