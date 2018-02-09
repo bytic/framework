195fd82ff518db2a5456240a7eb1f369788fbb79
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-02-09 12:33:47 +0200

    update Scripts View helper

--- src/Helpers/view/Scripts.php
+++ src/Helpers/view/Scripts.php
@@ -2,55 +2,107 @@
 
 namespace Nip\Helpers\View;
 
+/**
+ * Class Scripts
+ * @package Nip\Helpers\View
+ */
 class Scripts extends AbstractHelper
 {
-    protected $_files = [];
-    protected $_defaultPlaceholder = 'head';
-    protected $_pack = false;
 
+    /**
+     * @var array
+     */
+    protected $files = [];
+
+    protected $rawScripts = [];
+
+    protected $defaultPlaceholder = "head";
+
+    /**
+     * @param string $file
+     * @param bool $placeholder
+     * @return $this
+     */
     public function add($file, $placeholder = false)
     {
         return $this->addFile($file, 'add', $placeholder);
     }
 
+    /**
+     * @param $file
+     * @param string $direction
+     * @param bool $placeholder
+     * @return $this
+     */
     public function addFile($file, $direction = 'add', $placeholder = false)
     {
-        if (!$placeholder) {
-            $placeholder = $this->_defaultPlaceholder;
+        if ($placeholder === false || empty($placeholder)) {
+            $placeholder = $this->defaultPlaceholder;
         }
 
-        if (!is_array($this->_files[$placeholder])) {
-            $this->_files[$placeholder] = [];
+        if (!isset($this->files[$placeholder]) || !is_array($this->files[$placeholder])) {
+            $this->files[$placeholder] = [];
         }
 
         if ($direction == 'prepend') {
-            array_unshift($this->_files[$placeholder], $file);
+            array_unshift($this->files[$placeholder], $file);
         } else {
-            $this->_files[$placeholder][] = $file;
+            $this->files[$placeholder][] = $file;
         }
 
         return $this;
     }
 
+    /**
+     * @param $content
+     * @return $this
+     */
+    public function addRaw($content)
+    {
+        $this->rawScripts[] = $content;
+
+        return $this;
+    }
+
+    /**
+     * @param $file
+     * @param bool $placeholder
+     * @return Scripts
+     */
     public function prepend($file, $placeholder = false)
     {
         return $this->addFile($file, 'prepend', $placeholder);
     }
 
+    /**
+     * @return string
+     */
     public function __toString()
     {
-        return $this->render($this->_defaultPlaceholder);
+        return $this->render($this->defaultPlaceholder);
     }
 
+    /**
+     * @param bool $placeholder
+     * @return string
+     */
     public function render($placeholder = false)
     {
-        if (!$placeholder) {
-            $placeholder = $this->_defaultPlaceholder;
+        if ($placeholder == false && empty($placeholder)) {
+            $placeholder = $this->defaultPlaceholder;
         }
 
-        return $this->renderHMTL($this->_files[$placeholder]);
+        if (isset($this->files[$placeholder])) {
+            return $this->renderHMTL($this->files[$placeholder]);
+        }
+
+        return '';
     }
 
+    /**
+     * @param $files
+     * @return string
+     */
     public function renderHMTL($files)
     {
         $return = '';
@@ -66,73 +118,39 @@ class Scripts extends AbstractHelper
                 }
             }
 
-            if ($external) {
+
+            if (count($external)) {
                 foreach ($external as $file) {
                     $return .= $this->buildTag($file);
                 }
             }
 
-            $return .= $this->pack($internal);
+            foreach ($internal as $file) {
+                $return .= $this->buildTag($this->buildURL($file));
+            }
         }
 
         return $return;
     }
 
+    /**
+     * @param $path
+     * @return string
+     */
     public function buildTag($path)
     {
         return "<script type=\"text/javascript\" src=\"$path\"></script>\r\n";
     }
 
-    public function pack($files)
-    {
-        if ($files) {
-            if ($this->_pack === false) {
-                $return = '';
-
-                foreach ($files as $file) {
-                    $return .= $this->buildTag($this->buildURL($file));
-                }
-
-                return $return;
-            } else {
-                $lastUpdated = 0;
-                foreach ($files as $file) {
-                    $path = $this->getBasePath().$file.'.js';
-                    if (file_exists($path)) {
-                        $lastUpdated = max($lastUpdated, filemtime($path));
-                    }
-                }
-
-                $hash = md5(implode('', $files)).'.'.$lastUpdated;
-
-                $path = CACHE_PATH.'scripts/'.$hash;
-                if (!file_exists($path.'.js')) {
-                    $content = '';
-                    foreach ($files as $file) {
-                        $content .= file_get_contents($this->getBasePath().$file.'.js')."\r\n";
-                    }
-                    $packer = new \JavaScriptPacker($content, 'Normal', true, false);
-                    $content = $packer->pack();
-
-                    $file = new \Nip_File_Handler(['path' => $path.'.js']);
-                    $file->write($content);
-
-                    if ($file->gzip()) {
-                        $file->setPath($path.'.gz')->write();
-                    }
-                }
-
-                return '<script type="text/javascript" src="'.$this->buildURL($hash).'"></script>'."\r\n";
-            }
-        }
-
-        return false;
-    }
-
+    /**
+     * @param $source
+     * @return string
+     */
     public function buildURL($source)
     {
-        return $this->getBaseUrl().$source.(in_array(\Nip_File_System::instance()->getExtension($source),
-            ['js', 'php']) ? '' : '.js');
+        return $this->getBaseUrl()
+            .$source
+            .(in_array(\Nip_File_System::instance()->getExtension($source), ['js', 'php']) ? '' : '.js');
     }
 
     public function getBaseUrl()
@@ -150,5 +168,20 @@ class Scripts extends AbstractHelper
         $this->_pack = $pack;
 
         return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function renderRaw()
+    {
+        $return = '';
+        if (count($this->rawScripts)) {
+            $return .= '<script type="text/javascript">';
+            $return .= implode("\r\n", $this->rawScripts);
+            $return .= '</script>';
+        }
+
+        return $return;
     }
 }
