5656718055550de445baa47794c761b74b5e29b3
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-02-19 14:30:23 +0200

    add request function

--- src/functions/common.php
+++ src/functions/common.php
@@ -21,6 +21,26 @@ if (!function_exists('app')) {
     }
 }
 
+if (!function_exists('request')) {
+    /**
+     * Get an instance of the current request or an input item from the request.
+     *
+     * @param  array|string $key
+     * @param  mixed $default
+     * @return Nip\Request|string|array
+     */
+    function request($key = null, $default = null)
+    {
+        $request = app('request');
+        if (is_null($key)) {
+            return $request;
+        }
+        $value = $request->get($key);
+
+        return $value ? $value : $default;
+    }
+}
+
 function __shutdown()
 {
     db()->disconnect();
