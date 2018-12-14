9a7e5e0860720e53ecdcbd677d6b7133a5532e0c
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-12-14 22:59:57 +0200

    remove config method from functions. Moved to config repo

--- src/functions/application.php
+++ src/functions/application.php
@@ -54,28 +54,6 @@ if (!function_exists('env')) {
         return $value;
     }
 }
-if (!function_exists('config')) {
-    /**
-     * Get / set the specified configuration value.
-     *
-     * If an array is passed as the key, we will assume you want to set an array of values.
-     *
-     * @param  array|string $key
-     * @param  mixed $default
-     * @return \Nip\Config\Config|mixed
-     */
-    function config($key = null, $default = null)
-    {
-        if (is_null($key)) {
-            return app('config');
-        }
-        if (is_array($key)) {
-            return app('config')->set($key);
-        }
-
-        return app('config')->get($key, $default);
-    }
-}
 
 if (!function_exists('request')) {
     /**
